1cbded411eec18ff38e6058b756b3f8d8c9b1435
DavidCordobaPimienta <david.cordoba@grupokonecta.com>
2022-01-24 17:55:07 -0500

    Mejoras en el componente tipologías

--- resources/js/components/modulos/tipologias/Edit.vue
+++ resources/js/components/modulos/tipologias/Edit.vue
@@ -3,7 +3,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0"><strong>EDITAR CATEGORÍA</strong></h1>
+                    <h1 class="m-0"><strong>EDITAR TIPOLOGÍA</strong></h1>
                 </div>
                 <!-- /.col -->
             </div>
